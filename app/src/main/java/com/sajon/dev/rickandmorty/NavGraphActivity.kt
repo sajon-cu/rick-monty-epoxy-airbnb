365eb6bf9cb4ba57caec008cea4e691e7a15b1cb
sajon-cu <saifulislamsajon@gmail.com>
2024-05-07 11:50:22 +0600

    Back navigation with Jetpack Navigation components

--- app/src/main/java/com/sajon/dev/rickandmorty/NavGraphActivity.kt
+++ app/src/main/java/com/sajon/dev/rickandmorty/NavGraphActivity.kt
@@ -2,17 +2,27 @@ package com.sajon.dev.rickandmorty
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class NavGraphActivity : AppCompatActivity() {
-    lateinit var navController: NavController
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_graph)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
